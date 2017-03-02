8a45cf4a848c2525a93cb64aba572f66d554aa5b
qin <brokenmoon@126.com>
2017-03-02 22:28:06 +0800

    tc

---
+++ xbb/xbb/HeroInfo.h
@@ -0,0 +1,18 @@
+//
+//  HeroInfo.h
+//  xbb
+//
+//  Created by  qin on 2017/3/2.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HeroInfo : NSObject
+@property (nonatomic,copy) NSString *heroId;
+@property (nonatomic,copy) NSString *heroName;
+@property (nonatomic,copy) NSString *heroType;
+@property (nonatomic,copy) NSString *heroPos;
+@property (nonatomic,copy) NSString *heroFragmentFrom;
+@property (nonatomic,copy) NSString *heroDesc;
+@end
---
+++ xbb/xbb/HeroInfo.m
@@ -0,0 +1,13 @@
+//
+//  HeroInfo.m
+//  xbb
+//
+//  Created by  qin on 2017/3/2.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import "HeroInfo.h"
+
+@implementation HeroInfo
+
+@end
