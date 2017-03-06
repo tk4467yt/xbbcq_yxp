d5280feb0b00877ceb629e4d82aac5657101add1
qin <brokenmoon@126.com>
2017-03-06 21:20:33 +0800

    tc

--- xbb/xbb/AppDelegate.m
+++ xbb/xbb/AppDelegate.m
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone];
     [DbHandler initLocalDatabase];
     
     return YES;
