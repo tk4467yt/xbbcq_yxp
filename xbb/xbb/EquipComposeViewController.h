86f0bfc51fb7bcf3e5c44b38d0427cbaf4d9621c
qin <brokenmoon@126.com>
2017-03-09 23:07:05 +0800

    tc

---
+++ xbb/xbb/EquipComposeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EquipComposeViewController.h
+//  xbb
+//
+//  Created by  qin on 2017/3/9.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EquipComposeViewController : UIViewController
+
+@end
