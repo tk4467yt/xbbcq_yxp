372f00ebb6bebc17722c672bfebe4fab976261b0
qin <brokenmoon@126.com>
2017-03-17 22:27:10 +0800

    tc

--- xbb/xbb/EquipBriefInfoCollectionViewCell.m
+++ xbb/xbb/EquipBriefInfoCollectionViewCell.m
@@ -13,13 +13,16 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
+    
+    self.ivThumb.layer.masksToBounds=true;
+    self.ivThumb.layer.cornerRadius=5;
 }
 
 - (void)layoutSubviews
 {
     [super layoutSubviews];
     
-    [MyUtility applyMaskImageToImageView:self.ivThumb withImage:[UIImage imageNamed:@"equip_stencil"]];
+//    [MyUtility applyMaskImageToImageView:self.ivThumb withImage:[UIImage imageNamed:@"equip_stencil"]];//call is slow,so use cornerRadius
 }
 
 @end
