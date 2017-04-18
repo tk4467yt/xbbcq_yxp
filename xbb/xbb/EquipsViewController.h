1cd9337feee2da169dafe97e69ba64176590d360
qin <brokenmoon@126.com>
2017-04-18 22:42:02 +0800

    tc

---
+++ xbb/xbb/EquipsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EquipsViewController.h
+//  xbb
+//
+//  Created by  qin on 2017/4/18.
+//  Copyright © 2017年  qin. All rights reserved.
+//
+
+#import "MyRootViewController.h"
+
+@interface EquipsViewController : MyRootViewController
+
+@end
